8789ae3853fb2238d16e69603c8aecb5788b96e6
zhaozhiyangzzy <zzybry@163.com>
2024-10-09 06:01:29 -0700

    git test

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ static void startothers(void);
 static void mpmain(void)  __attribute__((noreturn));
 extern pde_t *kpgdir;
 extern char end[]; // first address after kernel loaded from ELF file
-
+// zzy add git test line
 // Bootstrap processor starts running C code here.
 // Allocate a real stack and switch to it, first
 // doing some setup required for memory allocator to work.
